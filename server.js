62f70c1e1dca3c9ebed5ff8cafa548940ce30da8
Deva <devaydv18@gmail.com>
2026-02-20 15:12:01 +0530

    server file

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const app = require("./src/app");
+const connectDB = require('./src/config/db')
+
+const PORT = process.env.PORT
+connectDB()
+
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
