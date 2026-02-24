d633d773307b2448b17d810ec28bf38a155a4cc0
Deva <devaydv18@gmail.com>
2026-02-25 01:09:30 +0530

    entry file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express =  require('express')
+const path = require("path");
 const authRouter = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 
@@ -8,6 +9,8 @@ const transactionRoutes = require('./routes/transactionRoutes')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+// Serve public folder
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use('/api/auth', authRouter)
 app.use('/api/accounts',accountRouter)
