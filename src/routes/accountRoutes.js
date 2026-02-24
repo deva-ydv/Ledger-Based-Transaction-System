d22616ea47eac457aae0176faa63aa544f43f69e
Deva <devaydv18@gmail.com>
2026-02-25 01:13:26 +0530

    account routes

---
+++ src/routes/accountRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const {authMiddleware} = require('../middlewares/authMiddleware')
+const { createAccount, getUserAccounts, getAccountBalance } = require('../controllers/accountController')
+
+const router = express.Router()
+
+
+
+// POST api/accounts
+// Create a new account 
+// protected route
+
+router.post('/', authMiddleware, createAccount)
+
+// GET api/accounts
+// get all the accounts logged in users
+// protected route
+router.get('/', authMiddleware, getUserAccounts)
+
+// GET api/accounts
+// get all the accounts logged in users
+// protected route
+
+
+router.get('/balance/:accountId', authMiddleware, getAccountBalance)
+
+
+
+module.exports = router
