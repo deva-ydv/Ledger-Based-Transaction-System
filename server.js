c84720f2acd4cbc42284c60c24c2eabeec0d6d0f
Deva <devaydv18@gmail.com>
2026-02-25 01:11:22 +0530

    server file

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const connectDB = require('./src/config/db')
 const PORT = process.env.PORT
 connectDB()
 
-app.get('/',(req,res)=>{
-    res.send("live")
-})
+
 
 app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
