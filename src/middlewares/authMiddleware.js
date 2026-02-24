4cd5c26c55513b6fdfc1f544be2beed782123ea8
Deva <devaydv18@gmail.com>
2026-02-25 01:12:51 +0530

    auth middlewares

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,3 +1,4 @@
+const tokenBlackListModel = require("../models/blackListModel");
 const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
@@ -7,6 +8,13 @@ async function authMiddleware(req, res, next) {
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized access or token is missing",
+    });
+  }
+  const isBlacklisted = await tokenBlackListModel.findOne({ token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({
+      message: "Unauthorized access, token is invalid",
     });
   }
 
@@ -29,29 +37,35 @@ async function authMiddleware(req, res, next) {
 }
 
 async function authSystemUserMiddleware(req, res, next) {
-    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+  const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
 
-    if (!token) {
-        return res.status(401).json({ message: "Token missing" });
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+  const isBlacklisted = await tokenBlackListModel.findOne({ token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({
+      message: "Unauthorized access, token is invalid",
+    });
+  }
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    // 1. Find the user
+    const user = await userModel.findById(decode.userId).select("+systemUser");
+
+    // 2. Check if user exists AND is a system user
+    if (!user || !user.systemUser) {
+      return res.status(401).json({
+        message: "system user not found",
+      });
     }
 
-    try {
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
-        // 1. Find the user
-        const user = await userModel.findById(decode.userId).select('+systemUser');
-        
-        // 2. Check if user exists AND is a system user 
-        if (!user || !user.systemUser) {
-            return res.status(401).json({
-                message: "system user not found",
-            });
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Invalid token" });
-    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
 
-module.exports = {authMiddleware, authSystemUserMiddleware};
+module.exports = { authMiddleware, authSystemUserMiddleware };
